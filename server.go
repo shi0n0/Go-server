8bfd89e7971602049cbe2c51652844356a11e0d8
shi0n0 <shiono.yui1080@gmail.com>
2023-03-08 22:17:45 +0900

    ボタン生成に切り替え

--- server.go
+++ server.go
@@ -2,30 +2,34 @@ package main
 
 import (
 	"html/template"
-	"net/http"
 	"math/rand"
+	"net/http"
 	"time"
 )
 
-
-func main() {
-	// テンプレートをパースする
-	tmpl := template.Must(template.ParseFiles("index.html"))
-
-	// HTTPハンドラーを定義する
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// ランダムな数字を生成する
-		rand.Seed(time.Now().UnixNano())
-		number := rand.Intn(101)
-
-		// HTMLテンプレートにランダムな数字を渡す
-		if err := tmpl.Execute(w, number); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
-
-	// Webサーバーを開始する
-	http.ListenAndServe(":8080", nil)
+// ランダムな数字を生成する関数
+func RandomNumber() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(101)
 }
 
+// テンプレートをレンダリングするハンドラー
+func renderTemplate(w http.ResponseWriter, r *http.Request) {
+	// テンプレート設定
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	// ボタンを押されたときにランダムな数字を生成
+	if r.Method == http.MethodPost {
+		randomNumber := RandomNumber()
+		tmpl.Execute(w, randomNumber)
+		return
+	}
+
+	// 初回アクセス時はランダムな数字を表示しない
+	tmpl.Execute(w, "xxx")
+}
+
+func main() {
+	http.HandleFunc("/", renderTemplate)
+	http.ListenAndServe(":8080", nil)
+}
